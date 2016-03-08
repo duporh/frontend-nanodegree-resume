70fa01b9438cde6a207dadb683f43b9c5ab695d9
Hari Durairaj <duporh@gmail.com>
2016-03-07 17:45:17 -0800

    Added Name and Title to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var formattedName = HTMLheaderName.replace("%data%", "Hari Durairaj");
+ var formattedRole = HTMLheaderRole.replace("%data%", "Software Engineer");
+ $('#header').append(formattedName);
+ $('#header').append(formattedRole);
